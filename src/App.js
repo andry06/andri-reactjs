9d764235db54c394e81efe29c2240a4ece964468
andry06 <andri.suryono@gmail.com>
2023-08-24 12:55:24 +0700

    menambah kan suryono

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload By Andri.
+          Edit <code>src/App.js</code> and save to reload By Andri Suryono.
         </p>
         <a
           className="App-link"
